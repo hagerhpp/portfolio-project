636ca9d25f8f8e1b3b78ad2c34e4c14f82cbd393
hagerhpp <loloahmeda18@gmail.com>
2024-09-13 01:46:23 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// 
+var options = {
+    strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+};
+
+var typed = new Typed(".text", options);
